c65cc2a03e7c6b34dbdd1b8d6d5e4ccc206fe52c
NegarShabab <negar.shabab@gmail.com>
2019-07-18 15:37:20 +1000

    Update hawkeyesec.sh

--- bin/hawkeyesec.sh
+++ bin/hawkeyesec.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+mkdir -p "${WORKSPACE}/reports/hawkeye"
+# Ignore failure to generating reports
+set +e
+chmod 777 ${WORKSPACE}/reports/hawkeye
+docker run --rm --volume ${WORKSPACE}:/target hawkeyesec/scanner-cli -j reports/hawkeye/${BUILD_TAG}.json
+set -e
